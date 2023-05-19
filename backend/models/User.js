adb35939faf0db70988c9cd70222be5e51d375b7
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-05-19 18:02:53 +0545

    user model with functions

---
+++ backend/models/User.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchmea = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "please enter your email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [4, "Password should be greater than 4 characters"],
+      select: false,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    address: [
+      {
+        country: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+        address1: {
+          type: String,
+        },
+        address2: {
+          type: String,
+        },
+        zipCode: {
+          type: String,
+        },
+        addressType: {
+          type: String,
+        },
+      },
+    ],
+    role: {
+      type: String,
+      default: "user",
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+//hashing password
+userSchmea.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+//compare password
+userSchmea.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//generating token for user
+userSchmea.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
