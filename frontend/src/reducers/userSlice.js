effdb3bbc66dc416780b1bac0e6c152b562ae90b
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-06-09 23:03:31 +0545

    redux toolkit actions setup for user

---
+++ frontend/src/reducers/userSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginRequest: (state, action) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload;
+    },
+    loginFail: (state, action) => {
+      state.loading = false;
+      state.isAuthenticated = false;
+      state.user = null;
+    },
+    clearError: (state, action) => {
+      state.error = null;
+    },
+  },
+});
+
+export const { loginRequest, loginFail, loginSuccess, clearError } =
+  counterSlice.actions;
+export default userSlice.reducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "../reducers/userSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: userSlice,
+  },
 });
