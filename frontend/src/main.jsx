09e757db0d4416c2cb6ec81d56cc5a2700632e02
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-06-10 16:57:26 +0545

    folder structure changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
