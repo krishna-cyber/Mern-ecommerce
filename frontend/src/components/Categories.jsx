f6f4a361640f3448be664f6c6d8d493b4e12fefe
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-06-23 17:31:32 +0545

    static data set for branding and svg

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import styles from "../styles/styles";
+import categoryData from "../static/data.js";
 
 const Categories = () => {
   return (
-    <div>Categories</div>
-  )
-}
+    <>
+      <div className={`${styles.section}`}>
+        <div className=' flex justify-between items-center'>
+          <h2>Categories</h2>
+          <div className=' bg-[smoke]'></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Categories
+export default Categories;
--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // product Data
 export const productData = [
   {
@@ -329,41 +331,77 @@ export const productData = [
   },
 ];
 
-
 export const categoryData = [
   {
     id: 1,
     name: "Computers & Laptop",
-    image_Url: 'https://www.istorebangladesh.com/images/thumbs/0000286_macbook-pro-m1_550.png'
+    image_Url:
+      "https://www.istorebangladesh.com/images/thumbs/0000286_macbook-pro-m1_550.png",
   },
   {
     id: 2,
     name: "Mobile & Tablets",
-    image_Url: 'https://m.media-amazon.com/images/I/31Vle5fVdaL.jpg'
+    image_Url: "https://m.media-amazon.com/images/I/31Vle5fVdaL.jpg",
   },
   {
     id: 3,
     name: "Music & Gaming",
-    image_Url: 'https://www.startech.com.bd/image/cache/catalog/headphone/havit/h763d/h763d-02-500x500.jpg'
+    image_Url:
+      "https://www.startech.com.bd/image/cache/catalog/headphone/havit/h763d/h763d-02-500x500.jpg",
   },
   {
     id: 4,
     name: "Shoes",
-    image_Url: 'https://mirzacdns3.s3.ap-south-1.amazonaws.com/cache/catalog/RLV0015/2-800x800.jpg'
+    image_Url:
+      "https://mirzacdns3.s3.ap-south-1.amazonaws.com/cache/catalog/RLV0015/2-800x800.jpg",
   },
   {
     id: 5,
     name: "Cloths",
-    image_Url: 'https://i0.wp.com/eccocibd.com/wp-content/uploads/2022/01/1802NL02_1.png?fit=550%2C550&ssl=1'
+    image_Url:
+      "https://i0.wp.com/eccocibd.com/wp-content/uploads/2022/01/1802NL02_1.png?fit=550%2C550&ssl=1",
   },
   {
     id: 6,
-    name : "gifts",
-    image_Url: "../../public/categoriesIcon/gift.jpeg"
+    name: "gifts",
+    image_Url: "../../public/categoriesIcon/gift.jpeg",
   },
   {
     id: 7,
-    name : "Others",
-    image_Url: "../../public/categoriesIcon/icon9.webp"
+    name: "Cosmetics and Beauty",
+    image_Url: "../../public/categoriesIcon/icon1.png",
+  },
+  {
+    id: 8,
+    name: "Others",
+    image_Url: "../../public/categoriesIcon/icon9.webp",
+  },
+];
+
+
+export const brandingData = [
+  {
+    id: 1,
+    name: "Free Shipping",
+    text: "For all orders over $99",
+    svg : "<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1H5.63636V24.1818H35" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M8.72763 35.0002C10.4347 35.0002 11.8185 33.6163 11.8185 31.9093C11.8185 30.2022 10.4347 28.8184 8.72763 28.8184C7.02057 28.8184 5.63672 30.2022 5.63672 31.9093C5.63672 33.6163 7.02057 35.0002 8.72763 35.0002Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M31.9073 35.0002C33.6144 35.0002 34.9982 33.6163 34.9982 31.9093C34.9982 30.2022 33.6144 28.8184 31.9073 28.8184C30.2003 28.8184 28.8164 30.2022 28.8164 31.9093C28.8164 33.6163 30.2003 35.0002 31.9073 35.0002Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M34.9982 1H11.8164V18H34.9982V1Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M11.8164 7.18164H34.9982" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path></svg>"
+  },
+  {
+    id: 2,
+    name: "Daily Surprise Offers",
+    text: "Saves up to 30% off",
+    svg: "<svg width="32" height="34" viewBox="0 0 32 34" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 17.4502C31 25.7002 24.25 32.4502 16 32.4502C7.75 32.4502 1 25.7002 1 17.4502C1 9.2002 7.75 2.4502 16 2.4502C21.85 2.4502 26.95 5.7502 29.35 10.7002" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10"></path><path d="M30.7 2L29.5 10.85L20.5 9.65" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path></svg>"
+  },
+  {
+    id: 3,
+    name: "Affordable Pricing",
+    text: "We offer competitive prices",
+    svg : "<svg width="32" height="35" viewBox="0 0 32 35" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13H5.5C2.95 13 1 11.05 1 8.5V1H7" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10"></path><path d="M25 13H26.5C29.05 13 31 11.05 31 8.5V1H25" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10"></path><path d="M16 28V22" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10"></path><path d="M16 22C11.05 22 7 17.95 7 13V1H25V13C25 17.95 20.95 22 16 22Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M25 34H7C7 30.7 9.7 28 13 28H19C22.3 28 25 30.7 25 34Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path></svg>"
+  },
+  {
+    id: 4,
+    name: "Secure Payment",
+    text: "100% secure payment",
+    svg: "<svg width="32" height="38" viewBox="0 0 32 38" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.6654 18.667H9.33203V27.0003H22.6654V18.667Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M12.668 18.6663V13.6663C12.668 11.833 14.168 10.333 16.0013 10.333C17.8346 10.333 19.3346 11.833 19.3346 13.6663V18.6663" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path><path d="M31 22C31 30.3333 24.3333 37 16 37C7.66667 37 1 30.3333 1 22V5.33333L16 2L31 5.33333V22Z" stroke="#FFBB38" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"></path></svg>"
   }
 ];
