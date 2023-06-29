8353b8ef5bf09f70dcf5de458524cff9bdbf0c4e
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-06-29 19:03:46 +0545

    useState useeffect for best deal

--- frontend/src/components/BestDeals.jsx
+++ frontend/src/components/BestDeals.jsx
@@ -1,8 +1,20 @@
 /** @format */
+import { productData } from "../static/data";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const BestDeals = () => {
+  useEffect(() => {
+    //sort from product data having highest sell 5 items in descending order
+
+    const sortedData = productData.sort((a, b) => {
+      return b.total_sell - a.total_sell;
+    });
+    //slice the first 5 items
+    const slicedData = sortedData.slice(0, 5);
+    setData(slicedData);
+  }, []);
+  const [data, setData] = useState([]);
   return <div>BestDeals</div>;
 };
 
