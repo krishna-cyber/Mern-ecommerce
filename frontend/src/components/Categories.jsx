cb89ca79a4ea11aa7318ccfba0e91e97286dde4f
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-06-29 14:04:22 +0545

    navlink accordingly categories clicke

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -4,24 +4,36 @@ import React from "react";
 import styles from "../styles/styles";
 import { categoryData } from "../static/data.js";
 import Branding from "./Branding";
+import { useNavigate } from "react-router-dom";
 
 const Categories = () => {
+  const navigate = useNavigate();
+  const handleClick = (category) => (e) => {
+    e.preventDefault();
+    navigate(`/products/${category.name}`);
+  };
   return (
     <>
       <Branding />
-      <div className={`${styles.section}`}>
+      <div className={`${styles.section} `}>
         <div className='bg-white flex flex-wrap items-center'>
           {categoryData &&
             categoryData.map((category) => {
               return (
                 <>
-                  <div className='flex items-center  p-2 w-[25%]'>
-                    <img
-                      className=' w-14 h-14'
-                      src={category.image_Url}
-                      alt=''
-                    />
-                    <p className=' font-semibold text-lg'>{category.name}</p>
+                  {" "}
+                  <div className=' w-[25%]  p-2'>
+                    <span
+                      className='flex items-center w-fit cursor-pointer'
+                      onClick={handleClick(category)}>
+                      {" "}
+                      <img
+                        className=' w-14 h-14'
+                        src={category.image_Url}
+                        alt=''
+                      />
+                      <p className=' font-semibold text-lg'>{category.name}</p>
+                    </span>
                   </div>
                 </>
               );
