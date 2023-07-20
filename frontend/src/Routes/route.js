1843f6af294237c762b5134d582d4c47ff17cb62
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-07-20 18:38:46 +0545

    route added at frontnd

---
+++ frontend/src/Routes/route.js
@@ -0,0 +1,23 @@
+/** @format */
+
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Categories from "../components/Categories";
+import BestDeals from "../components/BestDeals";
+import FeatureProduct from "../components/FeatureProduct.jsx";
+import EventSection from "../components/EventSection";
+import CompaniesProduct from "../components/CompaniesProduct";
+import Footer from "../components/Footer";
+import FAQ from "../components/FAQ";
+
+export {
+  Header,
+  Hero,
+  Categories,
+  BestDeals,
+  FeatureProduct,
+  EventSection,
+  CompaniesProduct,
+  Footer,
+  FAQ,
+};
