21f741154baf07e58786824180f2fcfaabc56d82
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-08-27 17:34:03 +0545

    Product database schema and model created

---
+++ backend/models/Products.js
@@ -0,0 +1,95 @@
+/** @format */
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
+    price: [
+      originalPrice: {
+        type: Number,
+        required: [true, "Please enter product original price"],
+      },
+      discountPrice: {
+        type: Number,
+      },
+    ],
+    description: {
+      type: String,
+      required: [true, "Please enter product description"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please enter product category"],
+    },
+    tags:{
+        type: String,
+    },
+    stock:{
+        type: Number,
+        required: [true, "Please enter product stock"],
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    reviews: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+                required: true,
+            },
+            rating: {
+                type: Number,
+
+            },
+            comment:{
+                type: String,
+
+            },
+            productId:{
+                type: string
+            },
+            createdAt:{
+                type: Date,
+                default: Date.now
+
+        }
+        }
+
+     ],
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    shop: {
+        type: Schema.Types.ObjectId,
+        ref: "Shop",
+        required: true,
+    },
+    sold:{
+        type: Number,
+        default: 0,
+    }
+
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
