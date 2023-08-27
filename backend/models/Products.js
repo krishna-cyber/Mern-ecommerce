9b120fb606f2d91a02227739d935637bdbf15dd9
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-08-27 18:04:47 +0545

    Product SCHEMA

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -10,12 +10,14 @@ const productSchema = new Schema(
       required: [true, "Please enter product name"],
     },
     price: [
-      originalPrice: {
-        type: Number,
-        required: [true, "Please enter product original price"],
-      },
-      discountPrice: {
-        type: Number,
+      {
+        originalPrice: {
+          type: Number,
+          required: [true, "Please enter product original price"],
+        },
+        discountPrice: {
+          type: Number,
+        },
       },
     ],
     description: {
@@ -26,66 +28,60 @@ const productSchema = new Schema(
       type: String,
       required: [true, "Please enter product category"],
     },
-    tags:{
-        type: String,
+    tags: {
+      type: String,
     },
-    stock:{
-        type: Number,
-        required: [true, "Please enter product stock"],
+    stock: {
+      type: Number,
+      required: [true, "Please enter product stock"],
     },
     images: [
-        {
-            public_id: {
-                type: String,
-                required: true,
-            },
-            url: {
-                type: String,
-                required: true,
-            }
-        }
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     reviews: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            rating: {
-                type: Number,
-
-            },
-            comment:{
-                type: String,
-
-            },
-            productId:{
-                type: string
-            },
-            createdAt:{
-                type: Date,
-                default: Date.now
-
-        }
-        }
-
-     ],
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        rating: {
+          type: Number,
+        },
+        comment: {
+          type: String,
+        },
+        productId: {
+          type: string,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
     rating: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     shop: {
-        type: Schema.Types.ObjectId,
-        ref: "Shop",
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: "Shop",
+      required: true,
     },
-    sold:{
-        type: Number,
-        default: 0,
-    }
-
-
+    sold: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
