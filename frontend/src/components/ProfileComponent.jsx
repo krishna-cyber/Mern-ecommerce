8bfe2df8b4c21b35faacd204a0a5bbc34146d472
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-09-12 22:44:42 +0545

    profile component

--- frontend/src/components/ProfileComponent.jsx
+++ frontend/src/components/ProfileComponent.jsx
@@ -1,9 +1,29 @@
 /** @format */
 
+import {
+  Box,
+  Card,
+  Container,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
 const ProfileComponent = () => {
-  return <div>ProfileComponent</div>;
+  return (
+    <Container
+      maxW={"container.xl"}
+      marginX={"auto"}
+      py={"10"}
+      display={"flex"}>
+      <Card width={"25%"}>
+        <CardBody>hello</CardBody>
+      </Card>
+      <Box>how</Box>
+    </Container>
+  );
 };
 
 export default ProfileComponent;
